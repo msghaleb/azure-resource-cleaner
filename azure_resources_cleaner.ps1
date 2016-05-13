adf0e7f1de6b7f1a746e287662951df05e81e60b
Mohamed Ghaleb <z003680t@MD12FXZC.ww002.siemens.net>
2016-05-13 16:09:34 +0200

    changing Get-AzureRmResource  to Get-AzureRMContext

--- azure_resources_cleaner.ps1
+++ azure_resources_cleaner.ps1
@@ -1,20 +1,20 @@
-<# 
-.SYNOPSIS 
+<#
+.SYNOPSIS
     Azure Service Cleaner
 
-.DESCRIPTION 
-    The script is to make it easier for you to clean up your Azure resources, without using the Azure portal. 
+.DESCRIPTION
+    The script is to make it easier for you to clean up your Azure resources, without using the Azure portal.
     All you need to do is to run the script and choose the resources you need to remove and press ok.
 
-.REQUIREMENTS 
-    You need to have Azure Powershell installed on your computer. 
+.REQUIREMENTS
+    You need to have Azure Powershell installed on your computer.
     https://azure.microsoft.com/en-us/documentation/articles/powershell-install-configure/
 
-.NOTES 
+.NOTES
     Author     : Mohamed Ghaleb - mohamed.ghaleb@siemens.com
 
-.LINK 
-    http://tfl09.blogspot.com 
+.LINK
+    http://tfl09.blogspot.com
 
 .TODO
     It would be nice if we can add the following as input from the user:
@@ -25,7 +25,7 @@
 #>
 
 
-# Setting the error reporting to Silent, as some resources will report as error due to dependences, which we will try again later. 
+# Setting the error reporting to Silent, as some resources will report as error due to dependences, which we will try again later.
 $ErrorActionPreference = "SilentlyContinue"
 
 # Defining a hashtable to collect all the resources choosen for removal.
@@ -38,7 +38,7 @@ $ErrorOccurred= 0
 # Check if you are logged into your Azure Subscription or even have Azure Powershell installed
 try
 {
-    Get-AzureRmResource | out-null 
+    Get-AzureRMContext | out-null 
 }
 catch
 {
@@ -46,8 +46,8 @@ catch
     $FailedItem = $_.Exception.ItemName
 }
 
-# Now we will take the Error Message and check what's going on to react. 
- 
+# Now we will take the Error Message and check what's going on to react.
+
 # If you have the Azure powershell installed but not login it yet it will get you to log in.
 if ($ErrorMessage -like '*login*')
 {
@@ -85,8 +85,8 @@ if ($ErrorOccurred -eq 0)
 {
     # Now we will get all your resources, and give you an Out Grid to choose them (use CTRL) and then import them into a Hashtable.
     Get-AzureRmResource | Select-Object ResourceName,ResourceType,ResourceGroupName, ResourceId | Out-GridView -Title "Select VM Deployments to Remove" -PassThru | ForEach-Object {
-  
-       $RmRetry.Add($($_.ResourceId), $($_.ResourceName)) 
+
+       $RmRetry.Add($($_.ResourceId), $($_.ResourceName))
 
     }
 
@@ -99,7 +99,7 @@ if ($ErrorOccurred -eq 0)
         $LoopBreaker = $RmRetry.Count + 9
         write-host "Azure Service Cleaner will start to remove your selected services. Please note this process may take time." -foregroundcolor "green"
         write-host " "
-        
+
         # Do while loop to start removing the resources, the loop will keep running untill:
         # 1- Either the LoopBreaker is less than the number of resources you have choosen (9 failuers in a row)
         # 2- or if the Hashtable is empty, which means all resources are remove.
@@ -112,10 +112,10 @@ if ($ErrorOccurred -eq 0)
 
              # Put the resource name in a valiable.
              $RmName = $RmRandom.Value | Out-String
-             
+
              # Try to remove the resource
              try
-             { 
+             {
                Remove-AzureRmResource -ResourceId $RmRandom.Name -Force
 
                # If the resource was removed successfully, we will get the user to know the good news, remove it from our hashtable and increment the Loopbreaker
@@ -125,7 +125,7 @@ if ($ErrorOccurred -eq 0)
              }
              catch
              {
-               # If we failed to remove the resource, we will decrement the Loopbreaker by one. 
+               # If we failed to remove the resource, we will decrement the Loopbreaker by one.
                $LoopBreaker = $LoopBreaker - 1
              }
           }
@@ -136,7 +136,7 @@ if ($ErrorOccurred -eq 0)
              write-host "It seems that the service(s) you are choosing do have dependences, please try to remove them in order e.g. the VM has to be removed before the NIC" -foregroundcolor "red"
              write-host " "
              write-host "Here is the list of resources I've failed to remove, you can try choosing different resources or try on using the Azure portal." -foregroundcolor "green"
-             $RmRetry | Format-Table @{L=’Resource Name';E={$_.Value}}, @{L=’Resource ID';E={$_.Name}}
+             $RmRetry | Format-Table @{L=ï¿½Resource Name';E={$_.Value}}, @{L=ï¿½Resource ID';E={$_.Name}}
              break
           }
         } # End of 'Do'
